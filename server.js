9f9e84eef73d0475b8ec9d35037adce0e4f230a4
muhammadha7an <153084928+muhammadha7an@users.noreply.github.com>
2026-09-03 17:03:29 +0500

    Prepare backend for Vercel deployment

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ dotenv.config();
 
 const app = express();
 
-connectDB();
-
 app.use(cors());
 app.use(express.json());
 
+// Connect MongoDB
+connectDB();
+
+// Test route
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -19,12 +21,18 @@ app.get("/", (req, res) => {
   });
 });
 
+// Auth routes
 const authRoutes = require("./routes/authRoutes");
 
 app.use("/api/auth", authRoutes);
 
+// Local development
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
